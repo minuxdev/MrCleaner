54e0974ee5019e33bae1a8363389a62597f83fdb
minux dev <minux.midi@gmail.com>
2021-09-13 01:19:14 +0200

    Created and binded functions to basic GUI

---
+++ app.py
@@ -0,0 +1,142 @@
+from tkinter import *
+from tkinter import filedialog
+from modules.gui import MrCleaner
+from modules.multiples import base_converter
+import os, shutil
+
+
+root = Tk()
+root.title("MrCleaner 1.0")
+root.geometry("700x500+300+100")
+root.minsize(400, 300)
+
+#   IMPORTING VARIABLES FROM GUI    #
+gui = MrCleaner(root)
+input = gui.input
+list_box = gui.list_box
+ctrl_del = gui.ctrl_del
+ctrl_rep = gui.ctrl_rep
+
+
+#   GLOBAL VARIABLES    #
+duplicated_files = []
+sizes = []
+
+
+#   FUNCTIONS   #
+def get_directory():
+
+    directory = filedialog.askdirectory(title="Select messy directory",
+                                        mustexist=True)
+    
+    input.delete(0, END)
+    input.insert(END, directory)
+
+    locate_duplicated_files()
+
+
+def locate_duplicated_files():
+    global duplicated_files, sizes
+
+    directory = input.get()
+    
+    found_files = []
+    files_path = []
+    files_sizes = []
+
+    for root, directories, files in os.walk(directory):
+        for file in files:
+            try:
+                new_file = os.path.join(root, file)
+                new_file_size = os.path.getsize(new_file)
+                found_files.append(file)
+                files_path.append(new_file)
+                files_sizes.append(new_file_size)
+
+            except FileNotFoundError as e:
+                pass
+            except OSError as e:
+                pass
+
+    for i in range(len(found_files)):
+        if found_files.count(found_files[i]) != 1:
+            duplicated_files.append(files_path[i])
+            sizes.append(files_sizes[i])
+
+    list_box.delete(0, END)
+
+    header = "====================== DUPLICATED FILES ======================"
+
+    list_box.insert(END, header)
+
+    for f in range(len(duplicated_files)):
+        file = f"{duplicated_files[f]} Size: {base_converter(files_sizes[f])}"
+        list_box.insert(END, file)
+
+
+def extract_file_from_duplicated(dub_files, sizes):
+
+    only_names = []
+    file_path = []
+    file_names = []
+    file_size = []
+
+    for i in range(len(dub_files)):
+        only_names.append(os.path.split(dub_files[i])[1])
+
+    for f in range(len(only_names)):
+        if only_names[f] not in file_names:
+            file_names.append(only_names[f])
+            file_path.append(dub_files[f])
+            file_size.append(sizes[f])
+
+    return file_path, file_size
+
+
+
+
+def remove_duplicated_files():
+    global duplicated_files, sizes
+
+    destiny_dir = filedialog.askdirectory(title="Choose backup directory", 
+                                        mustexist=True)
+    file_path, file_size = extract_file_from_duplicated(duplicated_files, sizes)
+
+    count = 0
+    try:
+        for i in range(len(duplicated_files)): 
+            if duplicated_files[i] in file_path:
+                shutil.copy(src=duplicated_files[i], dst=destiny_dir)
+        
+            if ctrl_del.get() == 1:
+                os.remove(duplicated_files[i])
+                count += 1
+    except:
+        pass
+
+    if ctrl_rep.get() == 1:
+        report_generator(duplicated_files, sizes)
+
+    list_box.delete(0, END)
+
+    for j in range(len(file_path)):
+        list_box.insert(END, (file_path[j], base_converter(file_size[j])))
+
+
+
+
+def report_generator(dub_files, sizes):
+
+    with open('report.txt', 'w') as rep:
+        rep.write('{:~^60}\n\n'.format('Duplicated files found'))
+        for l in range(len(dub_files)):
+            rep.write(f'{l+1} - {dub_files[l]}  -  {base_converter(sizes[l])}\n')
+
+
+
+#   COMMANDS    #
+dir_btn = gui.select_directory.config(command=get_directory)
+run_btn = gui.run_btn.config(command=remove_duplicated_files)
+
+
+root.mainloop()
---
+++ modules/gui.py
@@ -0,0 +1,79 @@
+from tkinter import (Checkbutton, Button, Entry, Frame, LabelFrame, Grid, 
+                    IntVar, Scrollbar, Listbox, Tk)
+
+
+class MrCleaner():
+    def __init__(self, master = None):
+        self.master = master
+
+        self.create_frames()
+        self.create_widgets()
+        self.place_widgets()
+    
+
+    def create_frames(self):
+        self.entry_frame = Frame(self.master)
+        self.control_frame = Frame(self.master)
+        self.output_frame = Frame(self.master)
+    
+
+    def create_widgets(self):
+        self.ctrl_del = IntVar()
+        self.ctrl_rep = IntVar()
+        
+        #   ENTRY  FRAME
+        self.select_directory = Button(self.entry_frame, text="Directory",
+                            width=12, font="Poppins 9")
+        self.input = Entry(self.entry_frame, bg="white", fg="black", 
+                            font="Poppins 9", justify="center")
+
+        #   CONTROL FRAME
+        self.run_btn = Button(self.control_frame, text="Run",
+                            width=12, font="Poppins 9")
+        self.delete = Checkbutton(self.control_frame, variable=self.ctrl_del,
+                            text="Delete Files", font="Poppins 9")
+
+        self.report = Checkbutton(self.control_frame, variable=self.ctrl_rep,
+                    text="Generate Report", font="Poppins 9")
+
+        #   OUTPUT  FRAME
+        self.scroll_v = Scrollbar(self.output_frame, orient="vertical")
+        self.scroll_h = Scrollbar(self.output_frame, orient="horizontal")
+        self.list_box = Listbox(self.output_frame, font="Poppins 9",
+                                yscrollcommand=self.scroll_v.set, 
+                                xscrollcommand=self.scroll_h.set)
+        self.scroll_h.config(command=self.list_box.xview)
+        self.scroll_v.config(command=self.list_box.yview)
+
+
+    def place_widgets(self):
+        self.entry_frame.grid(sticky="we", padx=10, pady=4)
+        self.control_frame.grid(row=1, sticky="we", padx=10, pady=4)
+        self.output_frame.grid(row=2, sticky="news", padx=10, pady=4)
+        
+        Grid.columnconfigure(self.master, 0, weight=1)
+        Grid.rowconfigure(self.master, 2, weight=1)
+
+        Grid.columnconfigure(self.entry_frame, 1, weight=1)
+        Grid.columnconfigure(self.output_frame, 1, weight=1)
+        Grid.rowconfigure(self.output_frame, 0, weight=1)
+
+
+        self.select_directory.grid(sticky="we", padx=4, pady=4)
+        self.input.grid(row=0, column=1, sticky="we", padx=4, pady=4)
+
+        self.run_btn.grid(row=0, column=0, sticky="ew", padx=4, pady=4)
+        self.delete.grid(row=0, column=1, padx=6)
+        self.report.grid(row=0, column=2, padx=6)
+
+        self.list_box.grid(columnspan=2,sticky="news", padx=4, pady=4)
+        self.scroll_v.grid(row=0, column=2, sticky="ns")
+        self.scroll_h.grid(row=1, columnspan=2, sticky="we")
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.minsize(400, 300)
+    root.title("MrCleaner")
+    app = MrCleaner(master=root)
+    root.mainloop()
